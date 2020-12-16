2b4bb1bcdf6d0ed662f3f8ed3811196253b253e4
Vitaliy5 <dydvitrom@ukr.net>
2020-12-16 11:11:21 +0200

    change default marks for players in main

--- src/com/dudnyk/Main.java
+++ src/com/dudnyk/Main.java
@@ -2,7 +2,7 @@ package com.dudnyk;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleRenderer render = new ConsoleRenderer(new String[] {"?", "+", "-"});
+        ConsoleRenderer render = new ConsoleRenderer(new String[] {" ", "X", "Y"});
         TicTacToeGame game = new TicTacToeGame(render);
         game.play();
     }
